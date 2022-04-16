efecad700d02b5c38ccfe799bfa7667a13ccb094
saikiran-rajagopal <saikiranbindh@gmail.com>
2022-04-16 10:52:10 +0530

    done

---
+++ src/SampleProg/Prog.java
@@ -0,0 +1,32 @@
+package SampleProg;
+
+import java.awt.List;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Prog {
+	
+	public static void main(String[] args) {
+//		String s = "Java, React, HTML, CSS";
+//		String[] str = s.split(",");
+//		for (String string : str) {
+//			System.out.println(string);
+//		}
+		
+//		String s = "hello";
+//		String rev = "";
+//		for (int i=s.length()-1; i>=0; i--) {
+//			
+//			rev = rev+s.charAt(i);
+//			System.out.println(rev);
+//		}
+		
+//		int a = 50;
+//		int b = 20;
+//		int max = a>b?a:b;
+//		System.out.println(max);
+		
+		
+	}
+
+}
